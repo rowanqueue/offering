bff6ef0d7f89c0c5ab6a109a3bfd174301a2da71
Christian Weinschenk <cbw307@nyu.edu>
2019-01-23 23:38:07 -0500

    MUSIC REORGANIZTION/NEW TRACKS

--- Assets/Scripts/TextSound.cs
+++ Assets/Scripts/TextSound.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class TextSound : MonoBehaviour {
     public static TextSound me;
-    public AudioClip typeSound;
+    public List<AudioClip> typeSound;
     public List<string> characters;
     public Dictionary<string, List<AudioClip>> speakerToVoice;
+    float nextSound;
+    public float duration;
 	// Use this for initialization
 	void Awake () {
         me = this;
         speakerToVoice = new Dictionary<string, List<AudioClip>>
         {
-            {"",new List<AudioClip>(){typeSound} }
+            //{"",new List<AudioClip>(){typeSound} }
+            {"", typeSound }
+
         };
 	}
     public void AddCharacter(string character, string filePosition)
@@ -28,14 +32,25 @@ public class TextSound : MonoBehaviour {
     }
     public void PlaySound(string speaker)
     {
-        AudioClip clip = typeSound;
+        AudioClip clip = typeSound[0];
         //until everyone has a voice, use smorble's
         if (speakerToVoice.ContainsKey(speaker))
         {
             clip = speakerToVoice[speaker][Random.Range(0, speakerToVoice[speaker].Count)];
         }
-
-        AudioManager.me.PlaySound(clip);
+        if(speaker == "")
+        {
+            if (Time.time > nextSound)
+            {
+                nextSound = Time.time + (duration*Random.Range(0.9f,1.1f));
+                AudioManager.me.PlaySound(clip);
+            }
+        }
+        else
+        {
+            AudioManager.me.PlaySound(clip);
+        }
+        
     }
 
 }
